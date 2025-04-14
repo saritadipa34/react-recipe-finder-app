1ddcd7b1362c64e2d51820ebcc5c10766cdcd28e
saritadipa34 <saritadipa34@gmail.com>
2025-04-14 14:31:29 +0530

    Remove meal functionality implemented

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import { FaRegHeart } from "react-icons/fa";
 import { MdVideoLibrary } from "react-icons/md";
 import { GiRiceCooker } from "react-icons/gi";
+import { FaHeart } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 
@@ -12,6 +13,7 @@ const Card=({card,onClick})=>{
      
             <img className="h-[110px] w-full " src={card && card.strMealThumb || "/food.jpeg"} alt="food" />
             <FaRegHeart onClick={onClick} className="absolute bottom-0 right-0" size={"20px"}/>
+            <FaHeart onClick={onClick} className="absolute bottom-0 right-0 text-red-500" size={"21px"}/>
             <h1 className="font-bold mt-3 mx-3 ">{card && card.strMeal || "meal"}</h1>
             <p className="mx-3 mb-1 font-semibold">{card &&  card.strArea || "area"}</p>
             <div className="flex gap-10 ">
--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -2,7 +2,15 @@ import { useEffect, useState } from 'react';
 import Card from './Card';
 const Favorites=()=>{
     const [addFav,setAddFav]=useState([]);    
+    const [removeItem,setRemoveItem]=useState([]);
     
+const handleRemove=(id)=>{
+    const removed=addFav.filter((fav)=>fav.idMeal !== id)
+    setAddFav(removed);
+    localStorage.setItem("card",JSON.stringify(removed));
+    console.log("remove",removed);
+}
+
     useEffect(()=>{
         const stored=localStorage.getItem("card");
         if(stored){
@@ -15,7 +23,7 @@ const Favorites=()=>{
             <h1 className='font-bold text-4xl bg-amber-400 my-4'>Your Favorote Items</h1>
             <div className='flex flex-wrap gap-5'>
                 {addFav.map((fav,i)=>{
-            return <Card  card={fav} key={i}/>
+            return <Card onClick={()=>handleRemove(fav.idMeal)} card={fav} id={fav.idMeal} key={i}/>
                 })}
             </div>
         </div>
